24c18186edc7357e442119f3c3d131779a672ef3
codyrhoward <codyrhoward19@gmail.com>
2020-08-01 16:46:41 -0400

    learn to play modal built, started some css.

--- maincode/app.js
+++ maincode/app.js
@@ -1,1 +1,16 @@
-console.log('Hi github')
+    //Grabbing Elements
+    const $openBtn = $('#openModal');
+    const $modal = $('#modal');
+    const $closeBtn = $('#close');
+    //Event Handlers
+    const openModal = (event) => {
+      $modal.css('display', 'block');
+    }
+    const closeModal = (event) => {
+      $modal.css('display', 'none');
+    }
+    //Event Listeners
+    $openBtn.on('click', openModal);
+    $closeBtn.on('click', closeModal);
+
+
